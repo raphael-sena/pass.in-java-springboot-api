a668116df484c678a4ff0d495a6b19b5e58c4090
Raphael Sena <rsenares1@gmail.com>
2024-04-03 18:06:12 -0300

    Project Created
    Database set, dependencies, configurations and instructures

---
+++ src/main/java/rocketseat/com/passin/PassInApplication.java
@@ -0,0 +1,14 @@
+package rocketseat.com.passin;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInApplication.class, args);
+	}
+
+}
